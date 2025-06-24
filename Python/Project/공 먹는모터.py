f12b91ca8760a1589693689ad22ccbb5fd820a6e
jeonchangyeon07 <jeonchangyeon6@gmail.com>
2025-06-24 18:56:34 +0900

    feat : 흡입모터 2개 제어 구현 - 모터를 한개 더 추가하여 흡입이 가능 하게끔 정방향 역방향 기능 구현

--- Python/Project/공 먹는모터.py
+++ Python/Project/공 먹는모터.py
@@ -1,17 +1,29 @@
 import RPi.GPIO as GPIO
 import time
 
-DIR_PIN = 23
-BRK_PIN = 24
-PWM_PIN = 12
+DIR1_PIN = 23
+BRK1_PIN = 24
+PWM1_PIN = 12
+
+DIR2_PIN = 17
+BRK2_PIN = 27
+PWM2_PIN = 13
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(DIR_PIN, GPIO.OUT)
-GPIO.setup(BRK_PIN, GPIO.OUT)
-GPIO.setup(PWM_PIN, GPIO.OUT)
 
-pwm = GPIO.PWM(PWM_PIN, 1000)
-pwm.start(0)
+GPIO.setup(DIR1_PIN, GPIO.OUT)
+GPIO.setup(BRK1_PIN, GPIO.OUT)
+GPIO.setup(PWM1_PIN, GPIO.OUT)
+
+GPIO.setup(DIR2_PIN, GPIO.OUT)
+GPIO.setup(BRK2_PIN, GPIO.OUT)
+GPIO.setup(PWM2_PIN, GPIO.OUT)
+
+
+pwm1 = GPIO.PWM(PWM1_PIN, 1000)
+pwm2 = GPIO.PWM(PWM1_PIN, 1000)
+pwm1.start(0)
+pwm2.start(0)
 
 try:
     while True:
@@ -19,26 +31,41 @@ try:
         
         if cmd == 'T':
             print("start - tennis mode (20%)")
-            GPIO.output(DIR_PIN, GPIO.HIGH)
-            GPIO.output(BRK_PIN, GPIO.LOW)
-            pwm.ChangeDutyCycle(20)
+            GPIO.output(DIR1_PIN, GPIO.HIGH)
+            GPIO.output(BRK1_PIN, GPIO.LOW)
+            pwm1.ChangeDutyCycle(20)
+            
+            GPIO.output(DIR2_PIN, GPIO.LOW)
+            GPIO.output(BRK2_PIN, GPIO.LOW)
+            pwm2.ChangeDutyCycle(20)
             
         elif cmd == 'B':
             print("start - Baseball mode (25%)")
-            GPIO.output(DIR_PIN, GPIO.HIGH)
-            GPIO.output(BRK_PIN, GPIO.LOW)
-            pwm.ChangeDutyCycle(25)
+            GPIO.output(DIR1_PIN, GPIO.HIGH)
+            GPIO.output(BRK1_PIN, GPIO.LOW)
+            pwm1.ChangeDutyCycle(25)
+            
+            GPIO.output(DIR2_PIN, GPIO.LOW)
+            GPIO.output(BRK2_PIN, GPIO.LOW)
+            pwm2.ChangeDutyCycle(25)
             
         elif cmd == 'P':
             print("start - Ping-pong mode (10%)")
-            GPIO.output(DIR_PIN, GPIO.HIGH)
-            GPIO.output(BRK_PIN, GPIO.LOW)
-            pwm.ChangeDutyCycle(10)
+            GPIO.output(DIR1_PIN, GPIO.HIGH)
+            GPIO.output(BRK1_PIN, GPIO.LOW)
+            pwm1.ChangeDutyCycle(10)
+            
+            GPIO.output(DIR2_PIN, GPIO.LOW)
+            GPIO.output(BRK2_PIN, GPIO.LOW)
+            pwm2.ChangeDutyCycle(10)
             
         elif cmd =='S':
             print("stop")
-            GPIO.output(BRK_PIN, GPIO.HIGH)
-            pwm.ChangeDutyCycle(0)
+            GPIO.output(BRK1_PIN, GPIO.HIGH)
+            pwm1.ChangeDutyCycle(0)
+            
+            GPIO.output(BRK1_PIN, GPIO.HIGH)
+            pwm2.ChangeDutyCycle(0)
             
         elif cmd =='Q':
             print("Quit")
@@ -48,5 +75,6 @@ except KeyboardInterrupt:
     pass
 
 finally:
-    pwm.stop()
+    pwm1.stop()
+    pwm2.stop()
     GPIO.cleanup()
