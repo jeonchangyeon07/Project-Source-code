068c3d2a1ab18e9a7def9de91016998396549e3d
jeonchangyeon07 <jeonchangyeon6@gmail.com>
2025-08-13 11:48:01 +0900

    feat: 모터 테스트 코드

--- Python/Project/motor/motor.py
+++ Python/Project/motor/motor.py
@@ -1,157 +1,123 @@
-import RPi.GPIO as GPIO
-import time
-import serial
+from gpiozero import PhaseEnableMotor, OutputDevice
+from time import sleep
 
-use_serial = False
 
-if use_serial:
-    ser = serial.Serial('/dev/serial0', baudrate=9600, timeout=1)
+INTAKE_MOTOR1_DIR_PIN = 23
+INTAKE_MOTOR1_BRK_PIN = 24
+INTAKE_MOTOR1_PWM_PIN = 12
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+INTAKE_MOTOR2_DIR_PIN = 17
+INTAKE_MOTOR2_BRK_PIN = 27
+INTAKE_MOTOR2_PWM_PIN = 13
 
-DIR1_PIN = 23
-BRK1_PIN = 24
-PWM1_PIN = 12
 
-DIR2_PIN = 17
-BRK2_PIN = 27
-PWM2_PIN = 13
+DRIVE_LEFT_DIR_PIN = 5
+DRIVE_LEFT_BRK_PIN = 6
+DRIVE_LEFT_PWM_PIN = 19
 
-GPIO.setup(DIR1_PIN, GPIO.OUT)
-GPIO.setup(BRK1_PIN, GPIO.OUT)
-GPIO.setup(PWM1_PIN, GPIO.OUT)
+DRIVE_RIGHT_DIR_PIN = 16
+DRIVE_RIGHT_BRK_PIN = 20
+DRIVE_RIGHT_PWM_PIN = 18
 
-GPIO.setup(DIR2_PIN, GPIO.OUT)
-GPIO.setup(BRK2_PIN, GPIO.OUT)
-GPIO.setup(PWM2_PIN, GPIO.OUT)
 
-pwm1 = GPIO.PWM(PWM1_PIN, 1000)
-pwm2 = GPIO.PWM(PWM2_PIN, 1000)
-pwm1.start(0)
-pwm2.start(0)
+intake_motor1 = PhaseEnableMotor(phase=INTAKE_MOTOR1_DIR_PIN, enable=INTAKE_MOTOR1_PWM_PIN)
+intake_brake1 = OutputDevice(INTAKE_MOTOR1_BRK_PIN, initial_value=True)
 
-DIR3_PIN = 5
-BRK3_PIN = 6
-PWM3_PIN = 19
+intake_motor2 = PhaseEnableMotor(phase=INTAKE_MOTOR2_DIR_PIN, enable=INTAKE_MOTOR2_PWM_PIN)
+intake_brake2 = OutputDevice(INTAKE_MOTOR2_BRK_PIN, initial_value=True)
 
-DIR4_PIN = 16
-BRK4_PIN = 20
-PWM4_PIN = 18
+drive_left = PhaseEnableMotor(phase=DRIVE_LEFT_DIR_PIN, enable=DRIVE_LEFT_PWM_PIN)
+drive_brake_left = OutputDevice(DRIVE_LEFT_BRK_PIN, initial_value=True)
 
-GPIO.setup(DIR3_PIN, GPIO.OUT)
-GPIO.setup(BRK3_PIN, GPIO.OUT)
-GPIO.setup(PWM3_PIN, GPIO.OUT)
+drive_right = PhaseEnableMotor(phase=DRIVE_RIGHT_DIR_PIN, enable=DRIVE_RIGHT_PWM_PIN)
+drive_brake_right = OutputDevice(DRIVE_RIGHT_BRK_PIN, initial_value=True)
 
-GPIO.setup(DIR4_PIN, GPIO.OUT)
-GPIO.setup(BRK4_PIN, GPIO.OUT)
-GPIO.setup(PWM4_PIN, GPIO.OUT)
 
-pwm3 = GPIO.PWM(PWM3_PIN, 1000)
-pwm4 = GPIO.PWM(PWM4_PIN, 1000)
-pwm3.start(0)
-pwm4.start(0)
+def start_intake(speed_percent):
+    speed = speed_percent / 100.0
+    print(f"흡입 모터 작동 (속도: {speed_percent}%)")
+    
+    intake_brake1.off()
+    intake_brake2.off()
+    
+    intake_motor1.forward(speed)
+    intake_motor2.backward(speed)
+
+def move_robot(left_speed_percent, right_speed_percent):
+    left_speed = left_speed_percent / 100.0
+    right_speed = right_speed_percent / 100.0
+    
+    drive_brake_left.off()
+    drive_brake_right.off()
+    
+    if left_speed >= 0:
+        drive_left.forward(left_speed)
+    else:
+        drive_left.backward(abs(left_speed))
+        
+    if right_speed >= 0:
+        drive_right.forward(right_speed)
+    else:
+        drive_right.backward(abs(right_speed))
+
+def stop_all_motors():
+    print("모든 모터 정지")
+    
+    intake_motor1.stop()
+    intake_motor2.stop()
+    drive_left.stop()
+    drive_right.stop()
+    
+    intake_brake1.on()
+    intake_brake2.on()
+    drive_brake_left.on()
+    drive_brake_right.on()
+
 
 try:
+    print("로봇 제어 프로그램을 시작합니다. (T/B/P/F/L/R/S/Q)")
+    stop_all_motors()
+
     while True:
-        cmd = None
+        cmd = input("명령 입력: ").strip().upper()
 
-        if use_serial:
-            if ser.in_waiting > 0:
-                cmd = ser.read().decode().strip().upper()
+        if cmd == 'T':
+            start_intake(20)
+        elif cmd == 'B':
+            start_intake(25)
+        elif cmd == 'P':
+            start_intake(10)
+        
+        elif cmd == 'F':
+            print("전진")
+            move_robot(10, 10)
+        elif cmd == 'L':
+            print("좌회전")
+            move_robot(10, 15)
+        elif cmd == 'R':
+            print("우회전")
+            move_robot(15, 10)
+            
+        elif cmd == 'S':
+            stop_all_motors()
+            
+        elif cmd == 'Q':
+            print("프로그램을 종료합니다.")
+            break
         else:
-            cmd = input("Enter command (T/B/P/F/B/L/R/S/Q): ").strip().upper()
-
-        if cmd:
-            print(f"Received: {cmd}")
-
-            if cmd == 'T':
-                print("start - tennis mode (20%)")
-                GPIO.output(DIR1_PIN, GPIO.HIGH)
-                GPIO.output(BRK1_PIN, GPIO.LOW)
-                pwm1.ChangeDutyCycle(20)
-
-                GPIO.output(DIR2_PIN, GPIO.LOW)
-                GPIO.output(BRK2_PIN, GPIO.LOW)
-                pwm2.ChangeDutyCycle(20)
-
-            elif cmd == 'B':
-                print("start - baseball mode (25%)")
-                GPIO.output(DIR1_PIN, GPIO.HIGH)
-                GPIO.output(BRK1_PIN, GPIO.LOW)
-                pwm1.ChangeDutyCycle(25)
-
-                GPIO.output(DIR2_PIN, GPIO.LOW)
-                GPIO.output(BRK2_PIN, GPIO.LOW)
-                pwm2.ChangeDutyCycle(25)
-
-            elif cmd == 'P':
-                print("start - ping-pong mode (10%)")
-                GPIO.output(DIR1_PIN, GPIO.HIGH)
-                GPIO.output(BRK1_PIN, GPIO.LOW)
-                pwm1.ChangeDutyCycle(10)
-
-                GPIO.output(DIR2_PIN, GPIO.LOW)
-                GPIO.output(BRK2_PIN, GPIO.LOW)
-                pwm2.ChangeDutyCycle(10)
-
-            elif cmd == 'F':
-                print("start - Forward")
-                GPIO.output(DIR3_PIN, GPIO.HIGH)
-                GPIO.output(BRK3_PIN, GPIO.LOW)
-                pwm3.ChangeDutyCycle(10)
-
-                GPIO.output(DIR4_PIN, GPIO.HIGH)
-                GPIO.output(BRK4_PIN, GPIO.LOW)
-                pwm4.ChangeDutyCycle(10)
-
-            elif cmd == 'L':
-                print("start - Left")
-                GPIO.output(DIR3_PIN, GPIO.HIGH)
-                GPIO.output(BRK3_PIN, GPIO.LOW)
-                pwm3.ChangeDutyCycle(10)
-
-                GPIO.output(DIR4_PIN, GPIO.HIGH)
-                GPIO.output(BRK4_PIN, GPIO.LOW)
-                pwm4.ChangeDutyCycle(15)
-
-            elif cmd == 'R':
-                print("start - Right")
-                GPIO.output(DIR3_PIN, GPIO.HIGH)
-                GPIO.output(BRK3_PIN, GPIO.LOW)
-                pwm3.ChangeDutyCycle(15)
-
-                GPIO.output(DIR4_PIN, GPIO.HIGH)
-                GPIO.output(BRK4_PIN, GPIO.LOW)
-                pwm4.ChangeDutyCycle(10)
-
-            elif cmd == 'S':
-                print("Stop all motors")
-                GPIO.output(BRK1_PIN, GPIO.HIGH)
-                GPIO.output(BRK2_PIN, GPIO.HIGH)
-                GPIO.output(BRK3_PIN, GPIO.HIGH)
-                GPIO.output(BRK4_PIN, GPIO.HIGH)
-
-                pwm1.ChangeDutyCycle(0)
-                pwm2.ChangeDutyCycle(0)
-                pwm3.ChangeDutyCycle(0)
-                pwm4.ChangeDutyCycle(0)
-
-            elif cmd == 'Q':
-                print("Quit")
-                break
-
-            else:
-                print("Invalid command")
+            print("알 수 없는 명령입니다.")
 
 except KeyboardInterrupt:
-    pass
+    print("\n사용자에 의해 프로그램이 중단되었습니다.")
 
 finally:
-    pwm1.stop()
-    pwm2.stop()
-    pwm3.stop()
-    pwm4.stop()
-    GPIO.cleanup()
-    if use_serial:
-        ser.close()
+    stop_all_motors()
+    intake_motor1.close()
+    intake_brake1.close()
+    intake_motor2.close()
+    intake_brake2.close()
+    drive_left.close()
+    drive_brake_left.close()
+    drive_right.close()
+    drive_brake_right.close()
+    print("GPIO 리소스 정리 완료.")
